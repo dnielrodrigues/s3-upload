a274a46d0392e3ac802851e5704cfa1446e7053b
Daniel Rodrigues <danielrodrigues.a113@gmail.com>
2016-12-13 10:51:06 -0300

    update config sample

--- config-sample.php
+++ config-sample.php
@@ -4,11 +4,14 @@
  * S3 PARAMS
  */
 
-define( S3_KEY , 'YOUR_KEY' );
+define( 'S3_KEY' , 'YOUR_KEY' );
 
-define( S3_SECRET , 'YOUR_SECRET' );
+define( 'S3_SECRET' , 'YOUR_SECRET' );
 
-define( S3_REGION , 'YOUR_REGION' );
+define( 'S3_REGION' , 'YOUR_REGION' );
 
-define( S3_BUCKET , 'YOUR_BUCKET' );
+define( 'S3_BUCKET' , 'YOUR_BUCKET' );
+
+define( 'S3_PREFIX_KEY' , 'YOUR_FOLDER' );
+
 
